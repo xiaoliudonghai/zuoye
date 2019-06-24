e8f2f045d98cc0449ad5d9f33a4869856322d170
liudonghai <2211869213@qq.com>
2019-06-24 14:41:14 +0800

    修改定饭

--- app/src/main/java/com/example/hlong/zuoye/api/Api.java
+++ app/src/main/java/com/example/hlong/zuoye/api/Api.java
@@ -8,6 +8,8 @@ import io.reactivex.Observable;
 import retrofit2.http.GET;
 
 public interface Api {
+    //的风格受到广泛
+    //爱上的人体育回家看了
     public String url="https://api.apiopen.top/";
     @GET("getJoke?page=1&count=20&type=video")
     Observable<Student> get();
